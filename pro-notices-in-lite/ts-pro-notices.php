fe3913ab7bb8487e3b6817f36562ea41702784ce
bhavik <bhavik@bhavik>
2018-04-18 15:31:33 +0530

    Modified the code fo dyamic changes
    Modified the code for dynamic changes

--- pro-notices-in-lite/ts-pro-notices.php
+++ pro-notices-in-lite/ts-pro-notices.php
@@ -8,7 +8,7 @@ class ts_pro_notices {
 	* @access public
 	* @since 3.5
 	*/
-	public static $plugin_name = "Order Delivery Date for WooCommerce";
+	public static $plugin_name = "";
 
 	/**
 	* Plugin's unique prefix
@@ -17,7 +17,7 @@ class ts_pro_notices {
 	* @since 3.5
 	*/
 
-	public static $plugin_prefix = 'orddd_lite';
+	public static $plugin_prefix = '';
 
 	/**
 	* Pro plugin's unique prefix
@@ -34,7 +34,11 @@ class ts_pro_notices {
 	* @since 3.5
 	*/
 
-	public function __construct() {
+	public function __construct( $ts_plugin_name = '', $ts_plugin_prefix = '', $ts_pro_plugin_prefix = '' ) {
+		self::$plugin_name   	 = $ts_plugin_name;
+		self::$plugin_prefix 	 = $ts_plugin_prefix;
+		self::$pro_plugin_prefix = $ts_pro_plugin_prefix;
+
 		//Initialize settings
 		register_activation_hook( __FILE__,  array( &$this, 'ts_notices_activate' ) );
 
@@ -58,19 +62,24 @@ class ts_pro_notices {
 	public static function ts_notices_of_pro() {
 		$activate_time = get_option ( self::$plugin_prefix . '_activate_time' );
         $sixty_days    = strtotime( '+60 Days', $activate_time );
-        $current_time  = current_time( 'timestamp' );
-
+		$current_time  = current_time( 'timestamp' );
+		$add_query_arguments = '';
+		$message             = '';
         if( !is_plugin_active( 'order-delivery-date/order_delivery_date.php' ) && 
             ( false === $activate_time || ( $activate_time > 0 && $current_time >= $sixty_days ) ) ) {
         	global $current_user ;
 			$user_id = $current_user->ID;
 			
-			// 15 (post delivery addon), 22 (ac lite), 30, 37 (post delivery addon) Days
 			if( ! get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore' ) ) {
 				$_link = 'https://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-woocommerce-pro-21/checkout?edd_action=add_to_cart&download_id=16&utm_source=wpnotice&utm_medium=first&utm_campaign=OrderDeliveryDateLitePlugin';
 			    $message = wp_kses_post ( __( 'Thank you for using Order Delivery Date for WooCommerce! Never login to your admin to check your deliveries by syncing the delivery dates to the Google Calendar from Order Delivery Date Pro for WooCommerce. <strong><a target="_blank" href= "' . $_link . '">Get it now!</a></strong>', 'order-delivery-date' ) );
 
-			    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_first_notice_ignore', '0' );
+				$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_first_notice_ignore', '0' );
+				
+				$class = 'updated notice-info point-notice one';
+				$style = 'position:relative';
+				$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+				printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 			}
 
 			if ( get_user_meta( get_current_user_id(),  self::$pro_plugin_prefix . '_first_notice_ignore' ) && 
@@ -79,13 +88,21 @@ class ts_pro_notices {
 
 				$first_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore_time' );
 				$fifteen_days = strtotime( '+15 Days', $first_ignore_time[0] );
+
 				if ( $current_time > $fifteen_days ) {
                     $post_purchase_link = 'https://www.tychesoftwares.com/store/premium-plugins/post-delivery-product-reviews-addon-order-delivery-date-woocommerce/checkout?edd_action=add_to_cart&download_id=278278&utm_source=wpnotice&utm_medium=second&utm_campaign=OrderDeliveryDateLitePlugin';
 
                     $message = wp_kses_post ( __( 'Send Product review emails to the customers on the next day of delivery using Post Delivery Product Reviews Addon for Order Delivery Date plugin. <strong><a target="_blank" href= "' . $post_purchase_link . '">Have it now!</a></strong>', 'order-delivery-date' ) );
 
-				    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_second_notice_ignore', '0' );
+					$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_second_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice two';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 				}
+
+				
 			} 
 
 			if ( get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore' ) &&  
@@ -95,15 +112,21 @@ class ts_pro_notices {
 				! is_plugin_active( 'woocommerce-abandoned-cart/woocommerce-ac.php' ) ) {
 				
 				$first_ignore_time = get_user_meta( get_current_user_id(),  self::$pro_plugin_prefix . '_first_notice_ignore_time' );
-				$orddd_fifteen_days = strtotime( '+15 Days', $orddd_first_ignore_time[0] );
+				$ts_fifteen_days = strtotime( '+15 Days', $first_ignore_time[0] );
 
-				if ( $current_time > $orddd_fifteen_days ) {
+				if ( $current_time > $ts_fifteen_days ) {
                     $orddd_wcal_lite_link = admin_url( '/plugin-install.php?s=abandoned+cart+tyche+softwares&tab=search&type=term' );
 
                     $message = wp_kses_post ( __( 'Boost your sales by recovering the abandoned carts with our FREE Abandoned Cart for WooCommerce plugin. <strong><a target="_blank" href= "'.$orddd_wcal_lite_link.'">Install it now.</a></strong>', 'order-delivery-date' ) );
 
-				    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_third_notice_ignore', '0' );
+					$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_third_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 				}
+
 			} 
 
 			if ( get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore' ) &&  
@@ -112,15 +135,20 @@ class ts_pro_notices {
 				( is_plugin_active( 'woocommerce-abandon-cart-pro/woocommerce-ac.php' ) || 
 				is_plugin_active( 'woocommerce-abandoned-cart/woocommerce-ac.php' ) ) ) {
 				
-				$orddd_first_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore_time' );
-				$orddd_fifteen_days = strtotime( '+15 Days', $orddd_first_ignore_time[0] );
+				$ts_first_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore_time' );
+				$ts_fifteen_days = strtotime( '+15 Days', $ts_first_ignore_time[0] );
 
-				if ( $current_time > $orddd_fifteen_days ){
+				if ( $current_time > $ts_fifteen_days ){
                     $_link = 'https://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-woocommerce-pro-21/checkout?edd_action=add_to_cart&download_id=16&utm_source=wpnotice&utm_medium=fourth&utm_campaign=OrderDeliveryDateLitePlugin';
 
                     $message = wp_kses_post ( __( 'Create Delivery Settings by Shipping Zones & Shipping Classes using Order Delivery Date Pro for WooCommerce plugin. <br>Use discount code "ORDPRO20" and grab 20% discount on the purchase of the plugin. The discount code is valid only for the first 20 customers. <strong><a target="_blank" href= "'.$_link.'">Purchase now</a></strong>.', 'order-delivery-date' ) );
 
-				    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
+					$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 				}
 			}
 
@@ -131,14 +159,19 @@ class ts_pro_notices {
 				! is_plugin_active( 'woocommerce-abandon-cart-pro/woocommerce-ac.php' ) && 
 				! is_plugin_active( 'woocommerce-abandoned-cart/woocommerce-ac.php' ) ) {
 
-				$orddd_second_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_second_notice_ignore_time' );
-				$orddd_seven_days = strtotime( '+7 Days', $orddd_second_ignore_time[0] );				
+				$ts_second_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_second_notice_ignore_time' );
+				$ts_seven_days = strtotime( '+7 Days', $ts_second_ignore_time[0] );				
 
-				if ( $current_time > $orddd_seven_days ) {
+				if ( $current_time > $ts_seven_days ) {
                     $orddd_wcal_lite_link = admin_url( '/plugin-install.php?s=abandoned+cart+tyche+softwares&tab=search&type=term' );
 
                     $message = wp_kses_post ( __( 'Boost your sales by recovering the abandoned carts with our FREE Abandoned Cart for WooCommerce plugin. <strong><a target="_blank" href= "'.$orddd_wcal_lite_link.'">Install it now.</a></strong>.', 'order-delivery-date' ) );
-				    $add_query_arguments = add_query_arg(  self::$pro_plugin_prefix . '_third_notice_ignore', '0' );
+					$add_query_arguments = add_query_arg(  self::$pro_plugin_prefix . '_third_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 				}
 			}
 
@@ -148,16 +181,24 @@ class ts_pro_notices {
                  ( is_plugin_active( 'woocommerce-abandon-cart-pro/woocommerce-ac.php' ) ||
                  is_plugin_active( 'woocommerce-abandoned-cart/woocommerce-ac.php' ) ) ) {
 
-            	$orddd_second_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_second_notice_ignore_time' );
-                $orddd_fifteen_days = strtotime( '+15 Days', $orddd_second_ignore_time[0] );
+            	$ts_second_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_second_notice_ignore_time' );
+                $ts_fifteen_days = strtotime( '+15 Days', $ts_second_ignore_time[0] );
 
-                if ( $current_time > $orddd_fifteen_days ) {
+                if ( $current_time > $ts_fifteen_days ) {
                     $_link = 'https://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-woocommerce-pro-21/checkout?edd_action=add_to_cart&download_id=16&utm_source=wpnotice&utm_medium=fourth&utm_campaign=OrderDeliveryDateLitePlugin';
 
                     $message = wp_kses_post ( __( 'Create Delivery Settings by Shipping Zones & Shipping Classes using Order Delivery Date Pro for WooCommerce plugin. <br>Use discount code "ORDPRO20" and grab 20% discount on the purchase of the plugin. The discount code is valid only for the first 20 customers. <strong><a target="_blank" href= "'.$_link.'">Purchase now</a></strong>.', 'order-delivery-date' ) );
 
-                    $add_query_arguments = add_query_arg(  self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
-                }
+					$add_query_arguments = add_query_arg(  self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
+				}
+				
+
+				
             }
 
             if ( get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore' ) &&
@@ -165,17 +206,22 @@ class ts_pro_notices {
                 get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_third_notice_ignore' ) &&
                 ! get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_fourth_notice_ignore' ) ) {
             	
-            	$orddd_third_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_third_notice_ignore_time' );
-            	$orddd_seven_days = strtotime( '+7 Days', $orddd_third_ignore_time[0] );
+            	$ts_third_ignore_time = get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_third_notice_ignore_time' );
+            	$ts_seven_days = strtotime( '+7 Days', $ts_third_ignore_time[0] );
                 
-                if ( $current_time > $orddd_seven_days ) {
+                if ( $current_time > $ts_seven_days ) {
                     $_link = 'https://www.tychesoftwares.com/store/premium-plugins/order-delivery-date-for-woocommerce-pro-21/checkout?edd_action=add_to_cart&download_id=16&utm_source=wpnotice&utm_medium=fourth&utm_campaign=OrderDeliveryDateLitePlugin';
 
                     $message = wp_kses_post ( __( 'Create Delivery Settings by Shipping Zones & Shipping Classes using Order Delivery Date Pro for WooCommerce plugin. <br>Use discount code "ORDPRO20" and grab 20% discount on the purchase of the plugin. The discount code is valid only for the first 20 customers. <strong><a target="_blank" href= "'.$_link.'">Purchase now</a></strong>.', 'order-delivery-date' ) );
 
-                    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
-                }
-            }
+					$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fourth_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
+				}
+			}
 
             if ( get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_first_notice_ignore' ) &&
 				 get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_second_notice_ignore' ) &&
@@ -184,24 +230,23 @@ class ts_pro_notices {
 				 ! get_user_meta( get_current_user_id(), self::$pro_plugin_prefix . '_fifth_notice_ignore' ) &&
 				 ! is_plugin_active( 'post-purchase-experience/post-purchase-experience.php' ) ) {
 
-				$orddd_fourth_ignore_time = get_user_meta( get_current_user_id(), '_fourth_notice_ignore_time' );
-				$orddd_seven_days = strtotime( '+7 Days', $orddd_fourth_ignore_time[0] );				
+				$ts_fourth_ignore_time = get_user_meta( get_current_user_id(), '_fourth_notice_ignore_time' );
+				$ts_seven_days = strtotime( '+7 Days', $ts_fourth_ignore_time[0] );				
 
-				if ( $current_time > $orddd_seven_days ) {
+				if ( $current_time > $ts_seven_days ) {
                    	$_link = 'https://www.tychesoftwares.com/store/premium-plugins/post-delivery-product-reviews-addon-order-delivery-date-woocommerce/checkout?edd_action=add_to_cart&download_id=278278&utm_source=wpnotice&utm_medium=fifth&utm_campaign=OrderDeliveryDateLitePlugin';
 
                     $message = wp_kses_post ( __( 'Receive feedbacks for your products from verified owners by sending them post delivery emails using Post Delivery Product Reviews addon of Order Delivery Date plugin. <strong><a target="_blank" href= "'.$_link.'">Have it now!</a></strong>', 'order-delivery-date' ) );
 
-				    $add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fifth_notice_ignore', '0' );
+					$add_query_arguments = add_query_arg( self::$pro_plugin_prefix . '_fifth_notice_ignore', '0' );
+					
+					$class = 'updated notice-info point-notice';
+					$style = 'position:relative';
+					$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
+					printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
 				}
 			} 
-
-		  	$class = 'updated notice-info point-notice';
-			$style = 'position:relative';
-			$cancel_button = '<a href="'.$add_query_arguments.'" class="dashicons dashicons-dismiss dashicons-dismiss-icon" style="position: absolute; top: 8px; right: 8px; color: #222; opacity: 0.4; text-decoration: none !important;"></a>';
-			printf( '<div class="%1$s" style="%2$s"><p>%3$s %4$s</p></div>', $class, $style, $message, $cancel_button );
-				
-        }
+		}
 	}
 
 	/**
@@ -243,4 +288,3 @@ class ts_pro_notices {
 	}
 }
 
-$ts_pro_notices = new ts_pro_notices();
